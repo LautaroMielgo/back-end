aa50a4404d8556bd9485661715595810bf7919c1
LautaroMielgo <103539468+LautaroMielgo@users.noreply.github.com>
2023-06-29 15:19:43 -0300

    Delete src directory

--- src/routes/authRouter/authRouter.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express');
-const authGoogleHandler  = require("../../handlers/authGoogleHandler/authGoogleHandler")
-
-const authRouter = Router();
-
-authRouter.post('/google', authGoogleHandler);
-
-module.exports = authRouter;
--- src/routes/mercadoPagoRouter/mercadoPagoRouter.js
+++
@@ -1,9 +0,0 @@
-
-const { Router } = require('express')
-
-const { postOrder } = require('../../handlers/mercadoPagoHandlers/mercadoPagoHadles');
-const mercadoPagoRouter = Router()
-
-mercadoPagoRouter.post('/', postOrder)
-
-module.exports = mercadoPagoRouter
